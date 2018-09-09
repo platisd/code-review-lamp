9321464e4632926735954d7135342f65c3a5763a
Dimitris Platis <platisd@users.noreply.github.com>
2018-09-09 18:40:17 +0200

    Dim colors for each review using HSV format

--- gerrit_watcher.ino
+++ gerrit_watcher.ino
@@ -6,8 +6,81 @@
 #include <ESP8266HTTPClient.h>
 #include "credentials.h"
 
+struct RGBColor {
+  RGBColor(int r = 0, int g = 0, int b = 0) : red{r}, green{g}, blue{b} {}
+  int red;
+  int green;
+  int blue;
+};
+
+struct HSVColor {
+  /**
+    @param h    Hue ranged          [0,360]
+    @param s    Saturation ranged   [0,100]
+    @param v    Value ranged        [0,100]
+  */
+  HSVColor(int h = 0, int s = 0, int v = 0) : hue{h}, saturation{s}, value{v} {}
+  int hue;
+  int saturation;
+  int value;
+
+  /**
+    Converts HSV colors to RGB that can be used for Neopixels
+    so that we can adjust the brightness of the colors.
+    Code adapted from: https://stackoverflow.com/a/14733008
+
+    @param hsv  The color in HSV format to convert
+    @return     The equivalent color in RGB
+  */
+  RGBColor toRGB() const {
+    // Scale the HSV values to the expected range
+    auto rangedHue = map(hue, 0, 360, 0, 255);
+    auto rangedSat = map(saturation, 0, 100, 0, 255);
+    auto rangedVal = map(value, 0, 100, 0, 255);
+
+    unsigned char region, remainder, p, q, t;
+
+    if (rangedSat == 0)
+    {
+      return {rangedVal, rangedVal, rangedVal};
+    }
+
+    region = rangedHue / 43;
+    remainder = (rangedHue - (region * 43)) * 6;
+
+    p = (rangedVal * (255 - rangedSat)) >> 8;
+    q = (rangedVal * (255 - ((rangedSat * remainder) >> 8))) >> 8;
+    t = (rangedVal * (255 - ((rangedSat * (255 - remainder)) >> 8))) >> 8;
+
+    switch (region)
+    {
+      case 0:
+        return {rangedVal, t, p};
+      case 1:
+        return {q, rangedVal, p};
+      case 2:
+        return {p, rangedVal, t};
+      case 3:
+        return {p, q, rangedVal};
+      case 4:
+        return {t, p, rangedVal};
+      default:
+        return {rangedVal, p, q};
+    }
+  }
+};
+
+#ifndef CREDENTIALS_IN_DIFFERENT_FILE
+const auto INTERNET_SSID = "your_ssid";
+const auto PASSWORD = "your_password";
+const String GERRIT_URL = "http://your_gerrit_url:8080";
+const auto GERRIT_USERNAME = "your_gerrit_username";
+const auto GERRIT_HTTP_PASSWORD = "your_gerrit_http_password";
+#endif
+
 const auto NEOPIXEL_PIN = 15;
 const auto NEOPIXEL_RING_SIZE = 16;
+const auto DIM_WINDOW = 10000UL;
 const auto CONNECTION_RETRIES = 20;
 const auto OPEN_REVIEWS_QUERY = "/a/changes/?q=status:open+is:reviewer";
 const String CHANGES_ENDPOINT = GERRIT_URL + "/a/changes/";
@@ -17,17 +90,18 @@ const auto ALL_REVIEWS_ASSIGNED_URL = GERRIT_URL + OPEN_REVIEWS_QUERY;
 const auto GERRIT_REVIEW_NUMBER_ATTRIBUTE = "_number";
 const auto GERRIT_REVIEW_APPROVAL_ATTRIBUTE = "Code-Review";
 const auto GERRIT_REVIEW_OWNERID_ATTRIBUTE = "_account_id";
-const auto WAIT_FOR_GERRIT_RESPONSE = 500;
+const auto WAIT_FOR_GERRIT_RESPONSE = 50;
 const auto ENOUGH_CONDUCTED_REVIEWS = 1;
 
-const auto KINDA_ORANGE = Adafruit_NeoPixel::Color(216, 81, 23);
-const auto MELLOW_YELLOW = Adafruit_NeoPixel::Color (196, 178, 41);
-const auto ALIEN_GREEN = Adafruit_NeoPixel::Color (174, 249, 52);
-const auto COOL_CYAN = Adafruit_NeoPixel::Color (43, 239, 177);
-const auto GREEK_BLUE = Adafruit_NeoPixel::Color (42, 62, 239);
-const auto GOTH_PURPLE = Adafruit_NeoPixel::Color (244, 68, 235);
+const HSVColor KINDA_ORANGE (27, 100, 100);
+const HSVColor MELLOW_YELLOW (58, 100, 100);
+const HSVColor ALIEN_GREEN (100, 100, 100);
+const HSVColor COOL_CYAN (184, 100, 100);
+const HSVColor GREEK_BLUE (227, 100, 100);
+const HSVColor GOTH_PURPLE (291, 100, 100);
+const HSVColor ALMOST_PINK (346, 100, 100);
 // Maps gerrit account ids with 32-bit neopixel lamp colors
-const std::map<const char*, uint32_t> LAMP_COLORS {
+std::map<String, HSVColor> LAMP_COLORS {
   {"1000000", KINDA_ORANGE},
   {"1000002", MELLOW_YELLOW},
   {"1000003", ALIEN_GREEN},
@@ -140,12 +214,18 @@ void connectToWifi() {
   Serial.println(" Connection success");
 }
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  connectToWifi();
+/**
+    Get all open reviews that our gerrit user has been assigned to,
+    then figure out whether enough developers have code-reviewed
+    each review. If this has not happened, return a color
+    (mapped to a developer) for every un-reviewed review.
+    Once a review has been reviewed adequately, remove ourselves
+    from it.
 
+    @return HSV colors for every unfinished code review
+*/
+std::vector<HSVColor> getColorsForUnfinishedReviews() {
+  std::vector<HSVColor> colorsToShow;
   // Get all reviews assigned to our user
   Serial.println("Getting all reviews assigned to us");
   auto reviews = getStreamAttribute(ALL_REVIEWS_ASSIGNED_URL, GERRIT_REVIEW_NUMBER_ATTRIBUTE);
@@ -155,23 +235,113 @@ void setup() {
     auto getChangeUrl = CHANGES_ENDPOINT + review;
     auto getReviewersUrl = CHANGES_ENDPOINT + review + REVIEWERS;
     auto approvals = getStreamAttribute(getReviewersUrl, GERRIT_REVIEW_APPROVAL_ATTRIBUTE);
-    auto finishedReviews = 0;
+    // Measure how many reviews have been conducted (i.e. approval is NOT `0`)
+    auto conductedReviews = 0;
     for (auto& approval : approvals) {
       Serial.println(approval);
       if (approval != "0") {
-        finishedReviews++;
+        conductedReviews++;
       }
     }
 
-    if (finishedReviews > ENOUGH_CONDUCTED_REVIEWS) {
+    if (conductedReviews > ENOUGH_CONDUCTED_REVIEWS) {
       Serial.printf("We got enough reviews in %s, removing ourselves\n", review.c_str());
       removeFromReview(getReviewersUrl, GERRIT_USERNAME);
     } else {
       auto ownerId = getStreamAttribute(getChangeUrl, GERRIT_REVIEW_OWNERID_ATTRIBUTE).front();
       Serial.printf("Owner ID: %s\n", ownerId.c_str());
+      if (LAMP_COLORS.find(ownerId) == LAMP_COLORS.end() ) {
+        // Unknown review owner
+        colorsToShow.push_back(ALMOST_PINK);
+      } else {
+        colorsToShow.push_back(LAMP_COLORS[ownerId]);
+      }
     }
   }
 
+  return colorsToShow;
+}
+
+/**
+   Dim the provided neopixels down until they are completely off
+   @param neopixels The neopixels to dim down
+*/
+void dimNeopixelsOff(Adafruit_NeoPixel& neopixels) {
+  static const auto DIM_DOWN_INTERVAL = 20UL;
+  //TODO: Fix without brightness
+  for (auto pixel = 0; pixel < neopixels.numPixels(); pixel++) {
+    neopixels.setPixelColor(pixel, 0, 0, 0);
+    neopixels.show();
+    delay(DIM_DOWN_INTERVAL);
+  }
+}
+
+/**
+   Set the specified RGB color to all the pixels
+
+   @param   neopixels   The neopixel structure to set color
+   @param   rgbColor    The RGB color to set the pixels
+*/
+void setAllPixelColor(Adafruit_NeoPixel& neopixels, RGBColor& rgbColor) {
+  for (auto pixel = 0; pixel < neopixels.numPixels(); pixel++) {
+    neopixels.setPixelColor(pixel, rgbColor.red, rgbColor.green, rgbColor.blue);
+  }
+}
+
+/**
+   Dim for all the supplied colors throughout the specified window
+   @param neopixels     The neopixel structure to dim
+   @param hsvColors     The HSV colors to be dimmed
+*/
+void dimWithColors(Adafruit_NeoPixel& neopixels, std::vector<HSVColor>& hsvColors) {
+  if (hsvColors.empty()) {
+    Serial.println("All code is reviewed, good job");
+    return;
+  }
+  // Dim every color within the designated time window
+  // The effect we are after is the more unfinished reviews
+  // the faster the neopixels will dim
+  static const auto TIME_BETWEEN_DIMS = 200UL;
+  auto timeSlotForEachColor = DIM_WINDOW / hsvColors.size();
+  for (const auto& hsvColor : hsvColors) {
+    auto rgb = hsvColor.toRGB();
+    Serial.println("Dimming up");
+    // The time slot has to be evenly divided among the intervals necessary
+    // to dim it all the way up and down
+    auto dimInterval = (timeSlotForEachColor / hsvColor.value) / 2;
+    // Dim up every pixel for the current color
+    for (auto intensity = 0; intensity < hsvColor.value; intensity++) {
+      // Get the RGB value of the currently dimmed HSV color
+      auto rgbColor = HSVColor(hsvColor.hue, hsvColor.saturation, intensity).toRGB();
+      setAllPixelColor(neopixels, rgbColor);
+      neopixels.show();
+      delay(dimInterval);
+    }
+    delay(TIME_BETWEEN_DIMS);
+    Serial.println("Dimming down");
+    // Dim down every pixel for the current color
+    for (auto intensity = hsvColor.value; intensity >= 0; intensity--) {
+      // Get the RGB value of the currently dimmed HSV color
+      auto rgbColor = HSVColor(hsvColor.hue, hsvColor.saturation, intensity).toRGB();
+      setAllPixelColor(neopixels, rgbColor);
+      neopixels.show();
+      delay(dimInterval);
+    }
+    delay(TIME_BETWEEN_DIMS);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  ring.begin();
+  ring.show(); // Initialize all pixels to 'off'
+
+  connectToWifi();
+  auto hsvColors = getColorsForUnfinishedReviews();
+  dimWithColors(ring, hsvColors);
+  Serial.println("Done");
 }
 
 void loop() {
